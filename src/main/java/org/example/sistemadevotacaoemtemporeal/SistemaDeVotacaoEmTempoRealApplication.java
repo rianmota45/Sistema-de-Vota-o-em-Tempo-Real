c51d8df48f5b0f07fb928306e89cf9ea1844bd67
Rian Mota <r14nm0t4@gmail.com>
2026-07-04 12:38:03 -0300

    Arquivos iniciais

---
+++ src/main/java/org/example/sistemadevotacaoemtemporeal/SistemaDeVotacaoEmTempoRealApplication.java
@@ -0,0 +1,13 @@
+package org.example.sistemadevotacaoemtemporeal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeVotacaoEmTempoRealApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaDeVotacaoEmTempoRealApplication.class, args);
+    }
+
+}
